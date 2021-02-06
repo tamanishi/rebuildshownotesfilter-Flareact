536fb1b22847e9ff4eba99122bd0d8f502235020
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-06 16:45:39 +0900

    add bootstrap

--- components/Episode.js
+++ components/Episode.js
@@ -4,8 +4,8 @@ import Moment from 'react-moment'
 export default function Episode({ episode }) {
   return (
     <div>
-      <span><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'>{ episode.title }</a></span> <span>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
-      <span>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } />)}</span>
+      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'>{ episode.title }</a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+      <span>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } key={ i } />)}</span>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Episode from '../components/Episode'
 import Header from '../components/Header'
+import { Container, Row, Col, Form, Input } from 'react-bootstrap';
 
 async function getEpisodes() {
   const endpoint = `https://tamanishi.net/rebuildshownotesfilter3/shownotes-json`
@@ -18,12 +19,18 @@ export async function getEdgeProps() {
 }
 
 export default function Index({ episodes }) {
+  const filterShownotes = (e) => {
+
+  }
+  
   return (
       <div>
-        <Header />
-        <p>
-          { episodes.map((episode, i) => <Episode episode={ episode } />)}
-        </p>
+        <Container>
+          <Header />
+          <Row><Col xs="3"><Form.Control type='text' placeholder='query' onChange={ filterShownotes } /></Col></Row>
+          {/* <form><input type='text' placeholder='query' onChange={ filterShownotes } /></form> */}
+            { episodes.map((episode, i) => <Episode episode={ episode } key={ i } />)}
+        </Container>
       </div>
   );
 }
