9aa47892e5e363ab8ef05726a44a4cef4f594182
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-13 21:34:46 +0900

    episode css

--- components/Episode.js
+++ components/Episode.js
@@ -6,9 +6,9 @@ import '../styles/global.css'
 
 export default function Episode({ episode, query }) {
   return (
-    <>
-      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='Highlight' searchWords={ [ query ] } textToHighlight={ unescape(episode.title).replaceAll('&nbsp;', ' ') } /></a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+    <div className='episode'>
+      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(episode.title).replaceAll('&nbsp;', ' ') } /></a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
       <span>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</span>
-    </>
+    </div>
   );
 }
