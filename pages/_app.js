d6dcfe7f26d58013d1ec9be5504fd8ff5e7be83c
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-13 18:10:54 +0900

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import '../styles/global.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+import TagManaget from 'react-gtm-module'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
   // Your custom stuff here
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-VTE9S5859Y' })
+  })
 
   return <Component {...pageProps} />;
 }
