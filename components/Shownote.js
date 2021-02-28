5bf0335c2406c7ff1153c07cfc31883e254a3f4f
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-28 20:29:38 +0900

    replaceAll -> replace

--- components/Shownote.js
+++ components/Shownote.js
@@ -4,6 +4,6 @@ import '../styles/global.css'
 
 export default function Shownote({ shownote, query }) {
   return (
-    <li><a href={ shownote.url } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(shownote.title).replaceAll('&nbsp;', ' ') } /></a></li>
+    <li><a href={ shownote.url } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(shownote.title).replace(/&nbsp;/g, ' ') } /></a></li>
   );
 }
